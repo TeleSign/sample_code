738ea609c6bef011bc762d4294eea71b732b14ed
Aidan Doherty <adoherty@telesign.com>
2022-09-08 15:03:41 -0700

    preliminary work for digest auth sample code

---
+++ python/rest/shared/ts_auth.py
@@ -0,0 +1,109 @@
+import base64
+from datetime import datetime
+import hmac
+import pytz
+import uuid
+
+
+def add_basic(request_properties, customer_id, api_key):
+    new_request_properties = request_properties
+    new_request_properties['headers']['Authorization'] = create_basic_auth_string(customer_id, api_key)
+    return new_request_properties['headers']
+
+def add_digest(request_properties, customer_id, api_key):
+    new_request_properties = request_properties
+    new_request_properties['path_url'] = get_path(new_request_properties['url'])
+    method = new_request_properties['method']
+    # Add headers to prepped_request
+    new_request_properties['headers']['x-ts-auth-method'] = new_request_properties['headers'].get('x-ts-auth-method', 'HMAC-SHA256')
+    new_request_properties['headers']['x-ts-nonce'] = uuid.uuid1().hex
+
+    if method in ("PUT", "POST"):
+        new_request_properties['headers']['Content-Type'] = new_request_properties['headers'].get('Content-Type', 'application/x-www-form-urlencoded')
+
+    if 'Date' not in new_request_properties['headers'] and 'x-ts-date' not in new_request_properties['headers']:
+        new_request_properties['headers']['Date'] = format_current_date()
+
+    # Generate signature
+    signature = generate_signature(customer_id, api_key, new_request_properties)
+
+    # Add Digest auth header
+    new_request_properties['headers']['Authorization'] = 'TSA ' + customer_id + ':' + signature
+
+    # Sort the headers in alpha order
+    new_headers = {k: request_properties['headers'][k] for k in sorted(request_properties['headers'])}
+
+    return new_headers
+
+def create_basic_auth_string(customer_id, api_key):
+    auth_string = customer_id + ":" + api_key
+    auth_string = base64.b64encode(auth_string.encode())
+    auth_string = auth_string.decode("utf-8")
+    auth_string = "Basic " + auth_string
+    return auth_string
+
+def format_current_date():
+    now = datetime.now(pytz.utc)
+    return now.strftime('%a, %d %b %Y %H:%M:%S %Z').replace('UTC', 'GMT')
+
+def generate_signature(customer_id, api_key, new_request_properties):
+    method = new_request_properties['method']
+
+    if new_request_properties['headers']['x-ts-auth-method'] == 'HMAC-SHA1' or new_request_properties['headers']['x-ts-auth-method'] == 'HMAC-SHA256':
+        auth_method = new_request_properties['headers']['x-ts-auth-method'].split('-')[1]
+    else:
+        auth_method = new_request_properties['headers']['x-ts-auth-method']
+
+    new_request_properties['body'] = new_request_properties.get('body', None)
+
+    if new_request_properties['body']:
+        fields = new_request_properties['body']
+    else:
+        fields = None
+
+    string_to_sign = method + '\n'
+    string_to_sign += new_request_properties['headers'].get('Content-Type', '') + '\n'
+    string_to_sign += new_request_properties['headers'].get('Date', '') + '\n'
+    string_to_sign += 'x-ts-auth-method:' + new_request_properties['headers']['x-ts-auth-method'] + '\n'
+
+    if 'x-ts-date' in new_request_properties['headers']:
+        string_to_sign += 'x-ts-date:' + new_request_properties['headers']['x-ts-date'] + '\n'
+
+    if 'x-ts-nonce' in new_request_properties['headers']:
+        string_to_sign = string_to_sign + 'x-ts-nonce:' + new_request_properties['headers']['x-ts-nonce'] + '\n'
+
+    if fields and fields is not None:
+        if new_request_properties['headers']['Content-Type'] == 'application/json':
+            string_to_sign += fields.decode('UTF-8') + '\n'
+        elif new_request_properties['headers']['Content-Type'] == 'application/x-www-form-urlencoded':
+            string_to_sign += fields + '\n'
+        else:
+            raise ValueError("Can't generate signature for this content type.")
+
+    string_to_sign += new_request_properties['path_url']
+
+    encoded_string_to_sign = string_to_sign.encode(encoding='utf-8')
+    decoded_api_key = base64.b64decode(api_key)
+    hmac_obj = hmac.new(decoded_api_key, encoded_string_to_sign, digestmod=auth_method)
+    signature = base64.b64encode(hmac_obj.digest()).decode('utf-8')
+    return signature
+
+def get_hostname(url):
+    parts = url.split('//')
+    return parts[1].split('/')[0]
+
+def get_path(url):
+    parts = url.split('//')
+    parts2 = parts[1].split('/')
+    parts2.pop(0)
+    path = ('/' + '/'.join(parts2))
+    return path
+
+def pretty_print_request(req):
+    print('{}\n{}\r\n{}\r\n\r\n{}\n'.format(
+        'Request:',
+        req.method + ' ' + req.url,
+        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
+
---
+++ python/rest/sms_verify/verify_with_own_code.py
@@ -0,0 +1,65 @@
+from requests import Request, Session
+import os
+from random import SystemRandom
+import sys
+
+sys.path.append('../shared/')
+import ts_auth
+
+
+# Generate a random number n digits in length using a system random.
+def random_with_n_digits(n):
+    return "".join(SystemRandom().choice('123456789') for _ in range(n))
+
+
+# Replace the defaults below with your Telesign authentication credentials from https://teleportal.telesign.com
+customer_id = os.getenv('CUSTOMER_ID', 'FFFFFFFF-EEEE-DDDD-1234-AB1234567890')
+api_key = os.getenv('API_KEY', 'TE8sTgg45yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==')
+
+# Set the REST API URL
+url = "https://rest-ww.telesign.com/v1/verify/sms"
+
+# Set the request inputs.
+# Set the default below to your test phone number. In your production code, update the phone number dynamically for each transaction.
+phone_number = os.getenv('PHONE_NUMBER', '15558675309')
+verify_code = random_with_n_digits(5)
+
+# Add all headers except auth headers
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Date': ts_auth.format_current_date()
+}
+
+# Create the payload
+payload = f"phone_number={phone_number}&verify_code={verify_code}"
+
+# Create session and prepped request
+s = Session()
+req = Request('POST', url, data=payload, headers=headers)
+prepped_request = req.prepare()
+
+# Add auth to prepped request
+request_properties = {
+    "method": prepped_request.method,
+    "headers": prepped_request.headers,
+    "body": prepped_request.body,
+    "url": prepped_request.url
+}
+
+# prepped_request.headers = ts_auth.add_basic(request_properties, customer_id, api_key)
+prepped_request.headers = ts_auth.add_digest(request_properties, customer_id, api_key)
+
+# Make the request and capture the response.
+response = s.send(prepped_request)
+
+# Display the request and response in the console for debugging purposes. In your production code, you would likely remove this.
+ts_auth.pretty_print_request(prepped_request)
+print(f"Response:\n{response.text}\n")
+
+# Display prompt to enter verification code in the console.
+# In your production code, you would instead collect the potential verification code from the end-user in your platform's interface.
+user_entered_verify_code = input("Please enter the verification code you were sent: ")
+if verify_code == user_entered_verify_code.strip():
+    print("Your code is correct.")
+else:
+    print("Your code is incorrect.")
