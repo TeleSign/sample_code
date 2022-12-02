84e0208c2d22da90696773ef45f6680f29e690d4
Aidan Doherty <adoherty@telesign.com>
2022-12-01 16:13:45 -0800

    changes to PHP sample code and adding .gitignnore

--- php/sdk/sms_verify/src/verify_with_own_code.php
+++ php/sdk/sms_verify/src/verify_with_own_code.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
-use telesign\sdk\messaging\MessagingClient;
+use telesign\enterprise\sdk\verify\VerifyClient;
 
 # Replace the defaults below with your Telesign authentication credentials.
 $customer_id = getenv('CUSTOMER_ID') ? getenv('CUSTOMER_ID') :'FFFFFFFF-EEEE-DDDD-1234-AB1234567890';
@@ -9,12 +9,12 @@ $api_key = getenv('API_KEY') ? getenv('API_KEY') :'TE8sTgg45yusumoN6BYsBVkh+yRJ5
 # Set the request inputs.
 # Set the default below to your test phone number. In your production code, update the phone number dynamically for each transaction.
 $phone_number = getenv('PHONE_NUMBER') ? getenv('PHONE_NUMBER'):'15551212';
-$message = "Hello world";
+$message = "Hello world from Telesign Enterprise SDK";
 $message_type = "ARN";
 
 # Make the request and capture the response.
-$messaging = new MessagingClient($customer_id, $api_key);
-$response = $messaging->message($phone_number, $message, $message_type);
+$verify_client = new VerifyClient($customer_id, $api_key);
+$response = $verify_client->sms($phone_number);
 
 # Display the response body in the console for debugging purposes. In your production code, you would likely remove this.
 print_r($response->json);
