83c8f780a356c655c4549d7dfaa60a899d819833
Aidan Doherty <adoherty@telesign.com>
2022-12-02 16:13:17 -0800

    work in progress csharp sample code

---
+++ csharp/sdk/sms/SendSMS/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Telesign;
+
+namespace SendSMS
+{
+    class SendSMS
+    {
+        static void Main(string[] args)
+        {
+            string customerId = "FFFFFFFF-EEEE-DDDD-1234-AB1234567890";
+            string apiKey = "TE8sTgg45yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==";
+            string phoneNumber = "15558675309";
+
+            if (System.Environment.GetEnvironmentVariable("CUSTOMER_ID") != null) {
+                customerId = System.Environment.GetEnvironmentVariable("CUSTOMER_ID");
+            }
+            
+            if (System.Environment.GetEnvironmentVariable("API_KEY") != null) {
+                apiKey = System.Environment.GetEnvironmentVariable("API_KEY");
+            }
+
+            if (System.Environment.GetEnvironmentVariable("PHONE_NUMBER") != null) {
+                phoneNumber = System.Environment.GetEnvironmentVariable("PHONE_NUMBER");
+            }
+
+            string message = "Hello world!";
+            string messageType = "ARN";
+
+            try
+            {
+                MessagingClient messagingClient = new MessagingClient(customerId, apiKey, "https://rest-ww.telesign.com");
+                RestClient.TelesignResponse telesignResponse = messagingClient.Message(phoneNumber, message, messageType);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+
+            return;
+
+        }
+    }
+}
---
+++ csharp/sdk/sms_verify/SendOTP/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Telesign;
+using TelesignEnterprise;
+
+namespace SendOTP
+{
+    class SendOTP
+    {
+        public static void Main(string[] args)
+        {
+            string customerId = "FFFFFFFF-EEEE-DDDD-1234-AB1234567890";
+            string apiKey = "TE8sTgg45yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==";
+            string phoneNumber = "15558675309";
+
+            if (System.Environment.GetEnvironmentVariable("CUSTOMER_ID") != null) {
+                customerId = System.Environment.GetEnvironmentVariable("CUSTOMER_ID");
+            }
+            
+            if (System.Environment.GetEnvironmentVariable("API_KEY") != null) {
+                apiKey = System.Environment.GetEnvironmentVariable("API_KEY");
+            }
+
+            if (System.Environment.GetEnvironmentVariable("PHONE_NUMBER") != null) {
+                phoneNumber = System.Environment.GetEnvironmentVariable("PHONE_NUMBER");
+            }
+
+            Random random = new Random();
+            int num = random.Next(100000);
+            string verifyCode = num.ToString();
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("verify_code", verifyCode);
+
+            try
+            {
+                VerifyClient verifyClient = new VerifyClient(customerId, apiKey);
+                RestClient.TelesignResponse telesignResponse = verifyClient.Sms(phoneNumber, parameters);
+
+                Console.WriteLine("Please enter your verification code:");
+                string code = Console.ReadLine().Trim();
+
+                if (verifyCode == code)
+                {
+                    Console.WriteLine("Your code is correct.");
+                }
+                else
+                {
+                    Console.WriteLine("Your code is incorrect.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
