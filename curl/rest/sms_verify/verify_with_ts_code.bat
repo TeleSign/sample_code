0d46a3c6b8bba24b24ec5a964186f7652b6962aa
Aidan Doherty <adoherty@telesign.com>
2023-01-31 16:16:57 -0800

    got the env variables working for auth

--- curl/rest/sms_verify/verify_with_ts_code.bat
+++ curl/rest/sms_verify/verify_with_ts_code.bat
@@ -1,4 +1,4 @@
-curl -u "$USERNAME:$API_KEY" --request POST \
+curl -u $CUSTOMER_ID:$API_KEY \
      --url https://rest-ww.telesign.com/v1/verify/sms \
      --header 'accept: application/json' \
      --header 'content-type: application/x-www-form-urlencoded' \
