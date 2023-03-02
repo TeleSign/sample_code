a143ad6d4e2da7553cf6c33dbb9457897da59ea1
Aidan Doherty <adoherty@telesign.com>
2023-03-01 16:09:32 -0800

    added fictitious phone number

--- node/sdk/sms/send_sms.js
+++ node/sdk/sms/send_sms.js
@@ -3,7 +3,7 @@ var TeleSignSDK = require('telesignsdk');
 const customerId = process.env.CUSTOMER_ID || "FFFFFFFF-EEEE-DDDD-1234-AB1234567890";
 const apiKey = process.env.API_KEY || "ABC12345yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==";
 
-const phoneNumber = process.env.PHONE_NUMBER || "Your test phone number with no special characters or spaces.";
+const phoneNumber = process.env.PHONE_NUMBER || "1234567890";
 
 const message = "Hello world";
 const messageType = "ARN";
