2a3656d2c1e174df4ebbd34929aacba424c9dec5
Aidan Doherty <adoherty@telesign.com>
2023-06-28 08:50:04 -0700

    fixing wrong param name for auth function

--- java/rest/sms_verify/src/main/java/sendOTP/App.java
+++ java/rest/sms_verify/src/main/java/sendOTP/App.java
@@ -9,13 +9,13 @@ public class App {
 		String customerId = System.getenv().getOrDefault("CUSTOMER_ID", "ABC1DE23-A12B-1234-56AB-AB1234567890");
         String apiKey = System.getenv().getOrDefault("API_KEY", "ABC12345yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==");
 
-        String phoneNumber = System.getenv().getOrDefault("PHONE_NUMBER", "1234567890");
+        String phoneNumber = System.getenv().getOrDefault("PHONE_NUMBER", "11234567890");
 
 		AsyncHttpClient client = new DefaultAsyncHttpClient();
 		client.prepare("POST", "https://rest-ww.telesign.com/v1/verify/sms")
 		  .setHeader("accept", "application/json")
 		  .setHeader("content-type", "application/x-www-form-urlencoded")
-		  .setHeader("authorization", createBasicAuthString(phoneNumber, apiKey))
+		  .setHeader("authorization", createBasicAuthString(customerId, apiKey))
 		  .setBody(String.format("is_primary=true&phone_number=%s", phoneNumber))
 		  .execute()
 		  .toCompletableFuture()
