26aadd8b8e140bbb70e08d35aa032c8438967802
adoherty <adoherty@telesign.com>
2024-02-01 10:37:37 -0800

    WIP sample code for PID Contact in python

--- python/sdk/phone_id/send_pn_to_pid_contact.py
+++ python/sdk/phone_id/send_pn_to_pid_contact.py
@@ -9,20 +9,26 @@ api_key = os.getenv('API_KEY', 'ABC12345yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh
 
 # Set the default below to your test phone number or pull it from an environment variable. 
 # In your production code, update the phone number dynamically for each transaction.
-phone_number = os.getenv('PHONE_NUMBER', '11234567890')
+#phone_number = os.getenv('PHONE_NUMBER', '11234567890')
+phone_number = '16262026728'
 
 # Instantiate a Phone ID client object.
-pid = PhoneIdClient(customer_id, api_key)
+pid = PhoneIdClient(customer_id, api_key,  rest_endpoint='https://phoneid-api-ci.c11.telesign.com')
 
 # Add the payload
+
+print("\nPhone number:{}\n".format(phone_number))
+
 payload = {  
     "addons": {
         "contact": {}
-    }
+    },
+    "phone_number": phone_number,
+    "tst-phoneid-fake": True
 }
 
 # Make the request and capture the response. 
-response = pid.phoneid(phone_number, payload)
+response = pid.phoneid(payload)
 
 # Display the response body in the console for debugging purposes. 
 # In your production code, you would likely remove this.
